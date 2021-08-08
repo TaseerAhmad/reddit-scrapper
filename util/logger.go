1b4a556c303d722584c2daf118b99aaaaf189463
TaseerAhmad <taseer.ahmadd@gmail.com>
2021-08-08 13:26:14 +0500

    Added a log writer

--- util/logger.go
+++ util/logger.go
@@ -1,16 +1,41 @@
 package util
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"time"
+	"os"
+	"reddit-scrapper/models"
 )
 
-func LogToJson(posts []byte) {
-	fiName := time.Now().String()
-	err := ioutil.WriteFile(fiName, posts, 0755)
+func LogToJson(posts []models.Post, fileName string) {
+	jsonData, err := json.MarshalIndent(posts, "", "  ")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 		return
 	}
+
+	fmt.Println("[INFO] Attempting to write scrapped data to", fileName)
+
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println("Unable to save scrapped data. Error: ", err.Error())
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(string(jsonData))
+	if err != nil {
+		fmt.Println("Unable to save scrapped data. Error: ", err.Error())
+		return
+	}
+
+	err = ioutil.WriteFile(fileName, jsonData, 0755)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	fmt.Println("[SUCCESS] Write to", fileName, "completed")
 }
